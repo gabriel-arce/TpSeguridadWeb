7657552402e2def70e1f4044ae4f682a41693cc0
gabriel-arce <gabriel_arce@hotmail.com>
2019-05-02 16:08:24 -0300

    Add files via upload
    2da version en paralelo

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,5 +1,32 @@
-var appDemo = angular.module('appDemo', []);
+var appDemo = angular.module('appDemo', ['ngRoute']);
 
+//Config
+appDemo.config(function($routeProvider, $locationProvider){
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller:  'mainCtrl'
+
+    })
+
+    .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller:  'aboutCtrl'
+        
+    })
+
+    .when('/contact', {
+        templateUrl: 'pages/contact.html',
+        controller:  'contactCtrl'
+        
+    })
+
+    $locationProvider.html5Mode(true);
+
+})
+
+//Instrumentos
 appDemo.controller('InstrumentosListadoCtrl', function ($scope) {
     $scope.model = [
         { marca: 'Fender', nombre: 'Fender Squier Vibe 60', precio: 400},
@@ -27,4 +54,20 @@ appDemo.controller('InstrumentosListadoCtrl', function ($scope) {
     $scope.retirar = function($index){
         $scope.model.splice($index, 1);
     }
+});
+
+// create the controller and inject Angular's $scope
+appDemo.controller('mainCtrl', function($scope){
+   $scope.message = 'Hello world, this is the home page!';
+
+})
+
+appDemo.controller('aboutCtrl', function($scope) {
+    $scope.message = 'Hi! This is the about page.';
+
+});
+
+appDemo.controller('contactCtrl', function($scope) {
+    $scope.message = 'Would you like to contact us?';
+
 });
